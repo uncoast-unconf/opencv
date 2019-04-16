6044d828cd71339ec3c8b596fd55720751b096b3
Geoffrey Thompson <gzthompson@gmail.com>
2019-04-16 11:36:24 -0500

    rename orb create object

--- R/orb.R
+++ R/orb.R
@@ -1,7 +1,7 @@
 #' @export
 #' @rdname opencv
 #' @param image a ocv image object
-ocv_create <- function(nfeatures = 500,
+ocv_orbcreate <- function(nfeatures = 500,
                           scaleFactor = 1.2,
                           nlevels = 8,
                           edgeThreshold = 31,
@@ -10,6 +10,6 @@ ocv_create <- function(nfeatures = 500,
                           scoreType = 1,
                           patchSize = 31,
                           fastThreshold = 20) {
-  
-  cvmat_create(nfeatures, scaleFactor, nlevels, edgeThreshold, firstLevel, WTA_K, scoreType, patchSize, fastThreshold)
+
+  cvmat_orbcreate(nfeatures, scaleFactor, nlevels, edgeThreshold, firstLevel, WTA_K, scoreType, patchSize, fastThreshold)
 }
