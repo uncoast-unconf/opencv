8d51f881f883fabff732ccec9292212ccb4c00ca
Geoffrey Thompson <gzthompson@gmail.com>
2019-04-16 11:34:17 -0500

    rename orb create function

--- src/orb.cpp
+++ src/orb.cpp
@@ -5,13 +5,13 @@ using namespace cv;
 using namespace std;
 
 // [[Rcpp::export]]
-XPtrOrb cvmat_create( int 	nfeatures = 500,
+XPtrOrb cvmat_orbcreate( int 	nfeatures = 500,
                        float 	scaleFactor = 1.2,
                        int 	nlevels = 8,
                        int 	edgeThreshold = 31,
                        int 	firstLevel = 0,
                        int 	WTA_K = 2){
-  
+
   Ptr<cv::ORB> obj = cv::ORB::create(nfeatures,
                              scaleFactor,
                              nlevels,
@@ -23,7 +23,7 @@ XPtrOrb cvmat_create( int 	nfeatures = 500,
                              20);
 
   Rcpp::XPtr<cv::ORB> p(obj, TRUE);
-  
+
   return(p);
 }
 
